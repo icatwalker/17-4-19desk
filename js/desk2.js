665d503b3b6e021a09789fa97951433ee87d7cee
icatwalker <m804396134@gmail.com>
2017-04-24 18:07:41 +0800

    添加座椅删除功能

--- js/desk2.js
+++ js/desk2.js
@@ -1,4 +1,7 @@
 $(function() {
+    //$("#workSpace").draggable();
+    //$("#people").draggable();
+
     flag=0;
     //console.log($("#people .people")[1]);
     //for(var i=0;i<=$("#people .people").length;i++){
@@ -14,8 +17,7 @@ $(function() {
         stop: function() {
 
             //var elem=`<div   class="chair img" id=${"d"+flag++}  ><span>${flag}号</span></div>`;
-            var elem="<div class='chair img' id="+"d"+flag++ +"><span>"+flag+"号</span></div>";
-
+            var elem="<div class='chair img' id="+"d"+flag++ +"><input type='text' value="+'d'+flag+"></div>";
             var id1=flag;
             id1--;
             console.log("id："+id1);
@@ -23,12 +25,24 @@ $(function() {
             $("#chairsArea").append(elem);
             //200为起始位置的200px,300为图片宽度
             mother.style.left=parseFloat(mother.style.left)-100;
-            $("#d"+id1).css("left",mother.style.left).css("top",mother.style.top).draggable();
+            $("#d"+id1).css("left",mother.style.left).css("top",mother.style.top).draggable({
+                drag:function(){
+                    CHAR1=$(this);
+                    $("#trash").droppable({
+                        drop: function (event, ui) {
+                            CHAR1.remove();
+                            //当降落时清空原来座位上的人名
+                            //event.target.remove();
+                        }
+                    });
+                }
+            }).bind(this);
             mother.style.left= -300+"px";
             mother.style.top=0;
 
         }
     });
+
     $(".people").draggable({//当拖动people时候 作为可以降落
         start:function(){
             //刚拖拽的时候将chair的data-guid属性清空
@@ -40,8 +54,7 @@ $(function() {
             $(".chair").droppable({
                 drop: function (event, ui) {
                     CHAIR = $(this);
-                    console.log($(this));
-                    $(this)
+                   $(this)
                         //当降落时清空原来座位上的人名
                         .attr("data-guid", "")
                         .addClass("ui-state-highlight")
@@ -90,3 +103,31 @@ $(function() {
 
 
 });
+//事件冒泡删除当前元素,chairarea做事件代理
+//当chair有people时候不允许删除 当 没有poeple时候才允许删除。
+$("#chairsArea").dblclick(function(e){
+    //当chair中只有一个元素input时候才允许删除
+    if(e.target.className.indexOf("chair")!=-1&&e.target.childNodes.length==1){
+        e.target.remove();
+    }
+});
+$("button.btn").on("click",function(){
+    console.log("提交");
+   console.log($("#chairsArea>.chair"));
+    var listItem=$("#chairsArea>.chair");
+    var chairCount=listItem.length;
+
+    var list=[];
+    for(var i=0;i<chairCount;i++){
+        list[i]={
+            "id":       listItem[i].id,"top":listItem[i].style.top,
+            "left":     listItem[i].style.left,
+            "chairName":listItem[i]
+        }
+    }
+    console.log(JSON.stringify(list));
+
+});
+
+
+
